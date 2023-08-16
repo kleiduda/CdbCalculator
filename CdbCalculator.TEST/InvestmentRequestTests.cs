c9ab79b67387a9a1a5382af21a56b1d9c4cd930a
kleiton <kleiduda@gmail.com>
2023-08-16 00:18:24 -0300

    test(applicatio): aumentando a cobertura na camada de application

---
+++ CdbCalculator.TEST/InvestmentRequestTests.cs
@@ -0,0 +1,35 @@
+﻿using CdbCalculator.Application.Arguments;
+using Xunit;
+
+namespace CdbCalculator.Tests
+{
+    public class InvestmentRequestTests
+    {
+        [Fact]
+        public void TestInvestmentRequest_InitialValueLessThanOrEqualToZero()
+        {
+            var request = new InvestmentRequest(0, 1);
+
+            Assert.False(request.IsValid);
+            Assert.Contains(request.Notifications, n => n.Message.Contains("Valor inicial não pode ser igual a zero."));
+        }
+
+        [Fact]
+        public void TestInvestmentRequest_PeriodInMonthsLessThanOrEqualToZero()
+        {
+            var request = new InvestmentRequest(100, 0);
+
+            Assert.False(request.IsValid);
+            Assert.Contains(request.Notifications, n => n.Message.Contains("Período em meses não pode ser igual a zero."));
+        }
+
+        [Fact]
+        public void TestInvestmentRequest_ValidValues()
+        {
+            var request = new InvestmentRequest(100, 1);
+
+            Assert.True(request.IsValid);
+            Assert.Empty(request.Notifications);
+        }
+    }
+}
---
+++ CdbCalculator.TEST/ServiceResultTests.cs
@@ -0,0 +1,50 @@
+﻿using CdbCalculator.Application;
+
+namespace CdbCalculator.Tests
+{
+    public class ServiceResultTests
+    {
+        [Fact]
+        public void TestStatusTrueWhenNoErrorAndResultNotNull()
+        {
+            var serviceResult = new ServiceResult<string>
+            {
+                Result = "teste calculadora cdb"
+            };
+
+            Assert.True(serviceResult.Status);
+        }
+
+        [Fact]
+        public void TestStatusFalseWhenThereIsAnError()
+        {
+            var serviceResult = new ServiceResult<string>
+            {
+                Result = "teste calculadora cdb",
+                Error = "Falhou algo"
+            };
+
+            Assert.False(serviceResult.Status);
+        }
+
+        [Fact]
+        public void TestStatusFalseWhenResultIsNull()
+        {
+            var serviceResult = new ServiceResult<string>();
+
+            Assert.False(serviceResult.Status);
+        }
+
+        [Fact]
+        public void TestStatusFalseWhenStatusExplicitlySetToFalse()
+        {
+            var serviceResult = new ServiceResult<string>
+            {
+                Result = "teste calculadora cdb",
+                Status = false
+            };
+
+            Assert.False(serviceResult.Status);
+        }
+    }
+}
