b47cb87cc96cc6b4339ad702b1a9f07435db2925
kleiton <kleiduda@gmail.com>
2023-08-15 23:47:19 -0300

    test(factoryStrategy): testando a factory impostos, usando theory junto com inlinedata para testar os cenarios juntos no mesmo metodo

---
+++ CdbCalculator.TEST/TaxStrategyFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using CdbCalculator.Domain.Strategy;
+
+namespace CdbCalculator.Tests
+{
+    public class TaxStrategyFactoryTests
+    {
+        [Theory]
+        [InlineData(1, typeof(ShortTermTaxStrategy))]
+        [InlineData(6, typeof(ShortTermTaxStrategy))]
+        [InlineData(7, typeof(MidTermTaxStrategy))]
+        [InlineData(12, typeof(MidTermTaxStrategy))]
+        [InlineData(13, typeof(LongTermTaxStrategy))]
+        [InlineData(24, typeof(LongTermTaxStrategy))]
+        [InlineData(25, typeof(VeryLongTermTaxStrategy))]
+        [InlineData(36, typeof(VeryLongTermTaxStrategy))]
+        public void GetTaxStrategy_ReturnsCorrectStrategy(int periodInMonths, Type expectedStrategyType)
+        {
+            var strategy = TaxStrategyFactory.GetTaxStrategy(periodInMonths);
+
+            Assert.IsType(expectedStrategyType, strategy);
+        }
+    }
+}
